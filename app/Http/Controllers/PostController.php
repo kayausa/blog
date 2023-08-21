105c6354d1f098aeac7a8b9d8609fae4546fcc96
EC2 Default User <usarole1@gmail.com>
2023-08-21 13:02:16 +0900

    postsにデータの追加,branch'dev_basis02'の作成'.phpファイルの書き換えを行った

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\Post;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+   
+   
+    //use宣言は外部にあるクラスをPostController内にインポートできる
+    
+    
+    /**
+     * Post一覧を表示する
+     * 
+     * @param Post Postモデル
+     * 
+     */
+     public function index(Post $post)
+     {
+         return view('posts/index')->with(['posts'=> $post->get()]);
+     }
+     
+}
+
+?>
